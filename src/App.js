24e7ab07a2879d0f12dea1ff2ad01ee30669a333
MauriUrsino <mauricioursino95@gmail.com>
2022-11-01 18:37:29 -0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={""} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
