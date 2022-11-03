fb7c6912e1b0aed0c7ff0b6af5f014c6c61eff5f
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-03 12:08:47 -0300

    new add CardProduct

---
+++ src/components/Ranked/index.js
@@ -0,0 +1,1 @@
+export { default as RankedStar } from './RankedStar.jsx';
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
