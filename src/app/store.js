feafe3dcfeac5506b6dd882fbf4a291f2402e2ff
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-10 01:46:32 -0300

    Se agregaron filtro por categoria

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import { categoryReducer } from './category';
 import { userReducer } from './user';
 
 export const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   reducer: {
     user: userReducer,
+    category: categoryReducer,
   },
 });
